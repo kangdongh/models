e880d74f58bb950abd59fe3bcf25debca6fa0015
donghyun Kang <kangdongh@snu.ac.kr>
2017-07-12 11:20:26 +0000

    hh

--- mobilenet_eval.sh
+++ mobilenet_eval.sh
@@ -1,3 +1,1 @@
-# !/bin/bash
-
-rm -rf object_detection/VOC0712/mobilenet_ckpts/eval; mkdir object_detection/VOC0712/mobilenet_ckpts/eval; CUDA_VISIBLE_DEVICES=3 python object_detection/eval.py --logtostderr --pipeline_config_path=object_detection/workspace/ssd_mobilenet_v1_pascal.config --checkpoint_dir=object_detection/VOC0712/mobilenet_ckpts/ --eval_dir=object_detection/VOC0712/mobilenet_ckpts/eval
+python object_detection/eval.py --logtostderr --pipeline_config_path=object_detection/workspace/ssd_mobilenet_v1_pascal.config --checkpoint_dir=object_detection/VOC0712/mobilenet_ckpts --eval_dir=object_detection/VOC0712/
